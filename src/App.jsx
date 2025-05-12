ef60933a331752673de80a9c8e3cbfac9451dc11
Marsh-bp <bharathpranesh2004@gmail.com>
2025-05-12 11:48:18 +0530

    deployment

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import Home from './pages/Home/home'
 import Login from './pages/Login/login'
 import Signup from './pages/SignUp/signup'
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom"
 
 
 const routes = (
   <Router>
     <Routes>
+      <Route path="/" element={<Navigate to="/login" />} />
       <Route path="/dashboard" exact element = {<Home />} />
       <Route path="/login" exact element = {<Login />} />
       <Route path="/signup" exact element = {<Signup />} />
